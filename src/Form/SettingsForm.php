00477ab8d92d73412f31c5212b9d60b46bd966a5
Julian Valero <jvalero@hiberus.com>
2019-04-01 15:44:03 +0200

    Modified settings form and changed inputs names

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -31,28 +31,27 @@ class SettingsForm extends ConfigFormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('az_blob_fs.settings');
-    dpm($config->get('account_key'));
-    $form['account_name'] = [
+    $form['az_blob_account_name'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Account Name'),
       '#maxlength' => 255,
       '#size' => 64,
-      '#default_value' => $config->get('account_name'),
+      '#default_value' => $config->get('az_blob_account_name'),
     ];
-    $form['account_key'] = [
+    $form['az_blob_account_key'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Account Key'),
       '#description' => $this->t('The Account Key is hidden in this field for security reason; If you need to change it, just add it again.'),
       '#maxlength' => 255,
       '#size' => 64,
     ];
-    $form['azure_blob_container_name'] = [
+    $form['az_blob_container_name'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Azure Blob Container name'),
       '#description' => $this->t('Create a blob container on from your storage account with public permissions for the container.'),
       '#maxlength' => 255,
       '#size' => 64,
-      '#default_value' => $config->get('azure_blob_container_name'),
+      '#default_value' => $config->get('az_blob_container_name'),
     ];
     return parent::buildForm($form, $form_state);
   }
@@ -70,14 +69,14 @@ class SettingsForm extends ConfigFormBase {
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
     $az_blob_fs_settings = $this->config('az_blob_fs.settings');
-    $account_key = $form_state->getValue('account_key');
+    $account_key = $form_state->getValue('az_blob_account_key');
     $az_blob_fs_settings
-      ->set('account_name', $form_state->getValue('account_name'))
-      ->set('azure_blob_container_name', $form_state->getValue('azure_blob_container_name'));
+      ->set('az_blob_account_name', $form_state->getValue('az_blob_account_name'))
+      ->set('az_blob_container_name', $form_state->getValue('az_blob_container_name'));
 
     if ($account_key != '') {
       $az_blob_fs_settings
-        ->set('account_key', $form_state->getValue('account_key'));
+        ->set('az_blob_account_key', $form_state->getValue('az_blob_account_key'));
     }
 
     $az_blob_fs_settings->save();
